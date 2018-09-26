873c854ee5bc579a7dadef6db41c997ff24e9ed7
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-26 09:59:56 -0400

    made some modification

--- news/tests.py
+++ news/tests.py
@@ -23,4 +23,34 @@ class EditorTestClass(TestCase):
     #     self.assertTrue(len(editors) > 0)    
 
 
+class ArticleTestClass(TestCase):
     
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.yvon= Editor(first_name = 'Yvonne', last_name ='Ouma', email ='yvon@gmail.com')
+        self.yvon.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.yvon)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datatime.strptime(test_date,'%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
+
--- news/views.py
+++ news/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 from django.http import HttpResponse
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
     return render( request,'welcome.html')
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
+    news =Article.todays_news()
 
-    return render(request,'all-news/today-news.html',{"date": date,})
+    return render(request,'all-news/today-news.html',{"date": date,"news":news})
 
 def convert_dates(date):
     # function that gets the weekday number for the date.
@@ -29,9 +31,13 @@ def past_days_news(request,past_date):
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
-        raise Http404()  
+        raise Http404()
+        assert False  
 
     if date == dt.date.today():
-        return redirect(news_of_day)
-   
-    return render(request,'all-news/past-news.html',{"date": date})   
+        return redirect(news_today)
+
+    news = Article.days_news(date)
+    return render(request,'all-news/past-news.html',{"date": date,"news": news})
+
+
